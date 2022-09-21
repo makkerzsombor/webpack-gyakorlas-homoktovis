9ed128cce8612fb06b42228874db1f6773e3c62d
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-21 18:12:49 +0200

    inputs done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.css';
-import "./main.css"
+import './main.css'
 
+let bogyoSzam = 0;
+let parlatSzam = 0;
+let szarazBogyoSzam = 0;
+let SzarazLemenySzam = 0;
+
+function init() {
+    
+}
+
+document.addEventListener('DOMContentLoaded',init);
